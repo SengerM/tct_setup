b6495a4fb639e0fe0837dd422656a64c4be8c584
Matias Senger <m.senger@hotmail.com>
2022-09-06 18:58:38 +0200

    First version of 1D scanning script and parsing script

---
+++ parse_waveforms.py
@@ -0,0 +1,91 @@
+from the_bureaucrat.bureaucrats import RunBureaucrat # https://github.com/SengerM/the_bureaucrat
+from pathlib import Path
+import pandas
+from huge_dataframe.SQLiteDataFrame import SQLiteDataFrameDumper, load_whole_dataframe # https://github.com/SengerM/huge_dataframe
+import sqlite3
+from signals.PeakSignal import PeakSignal, draw_in_plotly # https://github.com/SengerM/signals
+
+def parse_waveform(signal:PeakSignal):
+	parsed = {
+		'Amplitude (V)': signal.amplitude,
+		'Noise (V)': signal.noise,
+		'Rise time (s)': signal.rise_time,
+		'Collected charge (V s)': signal.peak_integral,
+		'Time over noise (s)': signal.time_over_noise,
+		'Peak start time (s)': signal.peak_start_time,
+		'Whole signal integral (V s)': signal.integral_from_baseline,
+		'SNR': signal.SNR
+	}
+	for threshold_percentage in [10,20,30,40,50,60,70,80,90]:
+		try:
+			time_over_threshold = signal.find_time_over_threshold(threshold_percentage)
+		except Exception:
+			time_over_threshold = float('NaN')
+		parsed[f'Time over {threshold_percentage}% (s)'] = time_over_threshold
+	for pp in [10,20,30,40,50,60,70,80,90]:
+		try:
+			time_at_this_pp = float(signal.find_time_at_rising_edge(pp))
+		except Exception:
+			time_at_this_pp = float('NaN')
+		parsed[f't_{pp} (s)'] = time_at_this_pp
+	return parsed
+
+def parse_waveforms(bureaucrat:RunBureaucrat, name_of_task_that_produced_the_waveforms_to_parse:str, continue_from_where_we_left_last_time:bool=True, silent:bool=True):
+	Quique = bureaucrat
+	
+	with Quique.handle_task('parse_waveforms', drop_old_data=not continue_from_where_we_left_last_time) as Quiques_employee:
+		try:
+			index_of_waveforms_already_parsed_in_the_past = set(load_whole_dataframe(Quiques_employee.path_to_directory_of_my_task/'parsed_from_waveforms.sqlite').index)
+		except FileNotFoundError:
+			index_of_waveforms_already_parsed_in_the_past = set()
+		
+		path_to_waveforms_file = Quiques_employee.path_to_directory_of_task(name_of_task_that_produced_the_waveforms_to_parse)/'waveforms.sqlite'
+		
+		sqlite_connection = sqlite3.connect(path_to_waveforms_file)
+		
+		if not silent:
+			print(f'Determining the total number of waveforms to be processed...')
+		sqlite_connection_cursor = sqlite_connection.cursor()
+		sqlite_connection_cursor.execute('SELECT max(n_waveform) from dataframe_table')
+		number_of_waveforms_to_process = sqlite_connection_cursor.fetchone()[0]+1
+		index_of_all_waveforms = set(range(number_of_waveforms_to_process))
+		index_of_waveforms_to_be_parsed = index_of_all_waveforms - index_of_waveforms_already_parsed_in_the_past
+		
+		if not silent:
+			print(f'{len(index_of_waveforms_to_be_parsed)} waveforms still need to be parsed. The others were already parsed beforehand. Will now proceed...')
+		
+		with SQLiteDataFrameDumper(Quiques_employee.path_to_directory_of_my_task/Path('parsed_from_waveforms.sqlite'), dump_after_n_appends = 100, dump_after_seconds = 60, delete_database_if_already_exists=False) as parsed_data_dumper: 
+			for i,n_waveform in enumerate(sorted(index_of_waveforms_to_be_parsed)):
+				if not silent:
+					print(f'Parsing n_waveform={n_waveform} ({i}/{len(index_of_waveforms_to_be_parsed)-1})')
+				waveform_df = pandas.read_sql_query(
+					f'SELECT * from dataframe_table where n_waveform=={n_waveform}',
+					sqlite_connection,
+				)
+				parsed_from_waveform = parse_waveform(PeakSignal(time=waveform_df['Time (s)'], samples=waveform_df['Amplitude (V)']))
+				parsed_from_waveform['n_waveform'] = n_waveform
+				parsed_from_waveform_df = pandas.DataFrame(
+					parsed_from_waveform,
+					index = [0],
+				).set_index(['n_waveform'])
+				parsed_data_dumper.append(parsed_from_waveform_df)
+
+if __name__=='__main__':
+	import argparse
+
+	parser = argparse.ArgumentParser(description='Cleans a beta scan according to some criterion.')
+	parser.add_argument('--dir',
+		metavar = 'path',
+		help = 'Path to the base measurement directory.',
+		required = True,
+		dest = 'directory',
+		type = str,
+	)
+
+	args = parser.parse_args()
+	parse_waveforms(
+		bureaucrat = RunBureaucrat(Path(args.directory)),
+		name_of_task_that_produced_the_waveforms_to_parse = 'TCT_1D_scan',
+		silent = False,
+		continue_from_where_we_left_last_time = True,
+	)
---
+++ scan_1D.py
@@ -0,0 +1,169 @@
+from the_bureaucrat.bureaucrats import RunBureaucrat # https://github.com/SengerM/the_bureaucrat
+from pathlib import Path
+from time import sleep
+import pandas
+import datetime
+from huge_dataframe.SQLiteDataFrame import SQLiteDataFrameDumper # https://github.com/SengerM/huge_dataframe
+from contextlib import nullcontext
+from progressreporting.TelegramProgressReporter import TelegramReporter # https://github.com/SengerM/progressreporting
+
+def TCT_1D_scan(
+		bureaucrat:RunBureaucrat,
+		the_setup,
+		positions:list,
+		acquire_channels:list,
+		n_triggers_per_position:int=1,
+		silent=True,
+		reporter:TelegramReporter=None,
+	):
+	"""Perform a 1D scan with the TCT setup.
+	
+	Arguments
+	----------
+	bureaucrat: RunBureaucrat
+		The bureaucrat that will handle the measurement.
+	the_setup:
+		An object to control the hardware.
+	positions: list of tuples
+		A list of tuples specifying the positions to measure. Each position
+		is a tuple of float of the form `(x,y,z)`.
+	acquire_channels: list of int
+		A list with the number of the channels to acquire from the oscilloscope.
+	n_triggers_per_position: int
+		Number of triggers to record at each position.
+	silent: bool, default True
+		If `True`, not messages will be printed. If `False`, messages will
+		be printed showing the progress of the measurement.
+	reporter: TelegramReporter
+		A reporter to report the progress of the script. Optional.
+	"""
+	Raúl = bureaucrat
+	
+	with Raúl.handle_task('TCT_1D_scan') as Raúls_employee:
+		if not silent:
+			print(f'Waiting to acquire exclusive control of the hardware...')
+		with the_setup.hold_control_of_bias(), the_setup.hold_signal_acquisition(), the_setup.hold_tct_control():
+			if not silent:
+				print(f'Control of hardware acquired!')
+			the_setup.configure_oscilloscope_for_two_pulses()
+			the_setup.set_laser_status(status='on') # Make sure the laser is on...
+			
+			report_progress = reporter is not None
+			with reporter.report_for_loop(len(positions)*n_triggers_per_position, f'{Raúl.run_name}') if report_progress else nullcontext() as reporter:
+				with SQLiteDataFrameDumper(
+					Raúls_employee.path_to_directory_of_my_task/Path('waveforms.sqlite'), 
+					dump_after_n_appends = 100, # Use this to limit the amount of RAM memory consumed.
+					dump_after_seconds = 60, # Use this to ensure the data is stored after some time.
+				) as waveforms_dumper:
+					with SQLiteDataFrameDumper(
+						Raúls_employee.path_to_directory_of_my_task/Path('measured_data.sqlite'), 
+						dump_after_n_appends = 100, # Use this to limit the amount of RAM memory consumed.
+						dump_after_seconds = 60, # Use this to ensure the data is stored after some time.
+					) as extra_data_dumper: # The `with` statement ensures all the data that was ever appended will be stored in disk.
+						n_waveform = 0
+						for n_position, target_position in enumerate(positions):
+							the_setup.move_to(**{xyz: n for xyz,n in zip(['x','y','z'],target_position)})
+							sleep(0.5) # Wait for any transient after moving the motors.
+							position = the_setup.get_stages_position()
+							for n_trigger in range(n_triggers_per_position):
+								if not silent:
+									print(f'Measuring: n_position={n_position}/{len(positions)-1}, n_trigger={n_trigger}/{n_triggers_per_position-1}...')
+								if report_progress:
+									reporter.update(1)
+								
+								the_setup.wait_for_trigger()
+								for n_channel in acquire_channels:
+									raw_data = the_setup.get_waveform(n_channel=n_channel)
+									raw_data_each_pulse = {}
+									for n_pulse in [1,2]:
+										raw_data_each_pulse[n_pulse] = {}
+										for variable in ['Time (s)','Amplitude (V)']:
+											if n_pulse == 1:
+												raw_data_each_pulse[n_pulse][variable] = raw_data[variable][:int(len(raw_data[variable])/2)]
+											if n_pulse == 2:
+												raw_data_each_pulse[n_pulse][variable] = raw_data[variable][int(len(raw_data[variable])/2):]
+										
+										# Because measuring bias voltage and current takes a long time (don't know why), I do the following ---
+										measure_slow_things_in_this_iteration = False
+										if 'last_time_slow_things_were_measured' not in locals() or (datetime.datetime.now()-last_time_slow_things_were_measured).seconds >= 11:
+											measure_slow_things_in_this_iteration = True
+											last_time_slow_things_were_measured = datetime.datetime.now()
+										
+										waveform_df = pandas.DataFrame(
+											{
+												'Time (s)': raw_data_each_pulse[n_pulse]['Time (s)'],
+												'Amplitude (V)': raw_data_each_pulse[n_pulse]['Amplitude (V)'],
+												'n_waveform': n_waveform
+											}
+										)
+										waveform_df.set_index('n_waveform', inplace=True)
+										extra_data_df = pandas.DataFrame(
+											{
+												'x (m)': position[0],
+												'y (m)': position[1],
+												'z (m)': position[2],
+												'When': datetime.datetime.now(),
+												'Bias voltage (V)': the_setup.measure_bias_voltage() if measure_slow_things_in_this_iteration else float('NaN'),
+												'Bias current (A)': the_setup.measure_bias_current() if measure_slow_things_in_this_iteration else float('NaN'),
+												'Laser DAC': the_setup.get_laser_DAC(),
+												'Temperature (°C)': the_setup.measure_temperature() if measure_slow_things_in_this_iteration else float('NaN'),
+												'Humidity (%RH)': the_setup.measure_humidity() if measure_slow_things_in_this_iteration else float('NaN'),
+												'n_waveform': n_waveform,
+												'n_position': n_position,
+												'n_trigger': n_trigger,
+												'n_channel': n_channel,
+												'n_pulse': n_pulse,
+											},
+											index = [0],
+										)
+										extra_data_df.set_index(
+											['n_waveform','n_position','n_trigger','n_channel','n_pulse'],
+											inplace=True
+										)
+										
+										waveforms_dumper.append(waveform_df)
+										extra_data_dumper.append(extra_data_df)
+										
+										n_waveform += 1
+
+########################################################################
+
+# The following things are defined here such that they can be imported from other scripts.
+
+DEVICE_CENTER = {
+	'x': -2.96578125e-3,
+	'y': 1.430810546875e-3,
+	'z': 67.3390039e-3,
+}
+SCAN_STEP = 10e-6 # meters
+SCAN_LENGTH = 333e-6 # meters
+SCAN_ANGLE_DEG = 90 # deg
+LASER_DAC = 0
+N_TRIGGERS_PER_POSITION = 55
+
+if __name__ == '__main__':
+	import numpy as np
+	import my_telegram_bots
+	from configuration_files.current_run import Alberto
+	from utils import create_a_timestamp
+	from TheSetup import connect_me_with_the_setup
+	import os
+	
+	with Alberto.handle_task('TCT_scans', drop_old_data=False) as task_bureaucrat:
+		Mariano = task_bureaucrat.create_subrun(create_a_timestamp() + '_' + input('Measurement name? ').replace(' ','_'))
+	
+		x = DEVICE_CENTER['x'] + np.arange(-SCAN_LENGTH/2,SCAN_LENGTH/2, SCAN_STEP)*np.cos(SCAN_ANGLE_DEG*np.pi/180)
+		y = DEVICE_CENTER['y'] + np.arange(-SCAN_LENGTH/2,SCAN_LENGTH/2, SCAN_STEP)*np.sin(SCAN_ANGLE_DEG*np.pi/180)
+		z = DEVICE_CENTER['z'] + 0*x + 0*y
+		positions = []
+		for i in range(len(y)):
+			positions.append( [ x[i],y[i],z[i] ] )
+		
+		TCT_1D_scan(
+			bureaucrat = Mariano,
+			the_setup = connect_me_with_the_setup(who=f'iv_curve.py PID:{os.getpid()}'),
+			positions = positions,
+			n_triggers_per_position = N_TRIGGERS_PER_POSITION,
+			acquire_channels = [1,4],
+			silent = False,
+		)

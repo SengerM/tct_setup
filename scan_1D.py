859b4749597a6916e437f1f9fa433b37540f89ec
Matias Senger <m.senger@hotmail.com>
2022-10-28 15:23:29 +0200

    Automated multiple devices measurements

--- scan_1D.py
+++ scan_1D.py
@@ -7,11 +7,12 @@ from huge_dataframe.SQLiteDataFrame import SQLiteDataFrameDumper, load_whole_dat
 from contextlib import nullcontext
 from progressreporting.TelegramProgressReporter import TelegramReporter # https://github.com/SengerM/progressreporting
 import threading
-from parse_waveforms import parse_waveforms
+from parse_waveforms import parse_waveform
 import plotly.express as px
 from utils import integrate_distance_given_path, kMAD, interlace
 from grafica.plotly_utils.utils import line
 import numpy as np
+from signals.PeakSignal import PeakSignal, draw_in_plotly # https://github.com/SengerM/signals
 
 def TCT_1D_scan(bureaucrat:RunBureaucrat, the_setup, positions:list, acquire_channels:list, n_triggers_per_position:int=1, silent=True, reporter:TelegramReporter=None):
 	"""Perform a 1D scan with the TCT setup.
@@ -46,23 +47,26 @@ def TCT_1D_scan(bureaucrat:RunBureaucrat, the_setup, positions:list, acquire_cha
 			if not silent:
 				print(f'Control of hardware acquired!')
 			the_setup.configure_oscilloscope_for_two_pulses()
-			the_setup.configure_oscilloscope_sequence_acquisition(n_sequences_per_trigger = n_triggers_per_position)
+			the_setup.configure_oscilloscope_sequence_acquisition(n_sequences_per_trigger = int(n_triggers_per_position))
 			the_setup.set_laser_status(status='on') # Make sure the laser is on...
 			
 			report_progress = reporter is not None
 			with \
 				reporter.report_for_loop(len(positions), f'{Raúl.run_name}') if report_progress else nullcontext() as reporter, \
-				SQLiteDataFrameDumper(Raúls_employee.path_to_directory_of_my_task/Path('waveforms.sqlite'), dump_after_n_appends = 1111, dump_after_seconds = 60) as waveforms_dumper, \
+				SQLiteDataFrameDumper(Raúls_employee.path_to_directory_of_my_task/Path('parsed_from_waveforms.sqlite'), dump_after_n_appends = 7777, dump_after_seconds = 60) as parsed_data_dumper, \
 				SQLiteDataFrameDumper(Raúls_employee.path_to_directory_of_my_task/Path('measured_data.sqlite'), dump_after_n_appends = 1111, dump_after_seconds = 60) as extra_data_dumper \
 			:
 				n_waveform = 0
 				for n_position, target_position in enumerate(positions):
 					the_setup.move_to(**{xyz: n for xyz,n in zip(['x','y','z'],target_position)})
 					sleep(0.5) # Wait for any transient after moving the motors.
-					position = the_setup.get_stages_position()
+
 					if not silent:
 						print(f'Measuring: n_position={n_position}/{len(positions)-1}...')
+					
 					the_setup.wait_for_trigger()
+					
+					position = the_setup.get_stages_position()
 					extra_data = {
 						'x (m)': position[0],
 						'y (m)': position[1],
@@ -75,10 +79,12 @@ def TCT_1D_scan(bureaucrat:RunBureaucrat, the_setup, positions:list, acquire_cha
 						'Humidity (%RH)': the_setup.measure_humidity(),
 						'n_position': n_position,
 					}
+					extra_data = pandas.DataFrame(extra_data, index=[0])
+					extra_data.set_index('n_position', inplace=True)
+					extra_data_dumper.append(extra_data)
+					
 					for n_channel in acquire_channels:
 						data_from_oscilloscope = the_setup.get_waveform(n_channel=n_channel)
-						if not isinstance(data_from_oscilloscope, list):
-							raise RuntimeError(f'I was expecting a list, but received an object of type {type(data_from_oscilloscope)}. If thi is a dictionary, probably the reason is that you have to set "Sampling mode → sequence" in the oscilloscope.')
 						for n_trigger,raw_data in enumerate(data_from_oscilloscope):
 							raw_data_each_pulse = {}
 							for n_pulse in [1,2]:
@@ -89,27 +95,26 @@ def TCT_1D_scan(bureaucrat:RunBureaucrat, the_setup, positions:list, acquire_cha
 									if n_pulse == 2:
 										raw_data_each_pulse[n_pulse][variable] = raw_data[variable][int(len(raw_data[variable])/2):]
 								
-								waveform_df = pandas.DataFrame(
-									{
-										'Time (s)': raw_data_each_pulse[n_pulse]['Time (s)'],
-										'Amplitude (V)': raw_data_each_pulse[n_pulse]['Amplitude (V)'],
-										'n_waveform': n_waveform
-									}
+								parsed_from_waveform = parse_waveform(
+									PeakSignal(
+										time = raw_data_each_pulse[n_pulse]['Time (s)'], 
+										samples = raw_data_each_pulse[n_pulse]['Amplitude (V)']
+									)
 								)
-								waveform_df.set_index('n_waveform', inplace=True)
-								
-								extra_data['n_waveform'] = n_waveform
-								extra_data['n_trigger'] = n_trigger
-								extra_data['n_pulse'] = n_pulse
-								extra_data['n_channel'] = n_channel
-								extra_data_df = pandas.DataFrame(extra_data, index=[0])
-								extra_data_df.set_index(
+								parsed_from_waveform['n_waveform'] = n_waveform
+								parsed_from_waveform['n_position'] = n_position
+								parsed_from_waveform['n_trigger'] = n_trigger
+								parsed_from_waveform['n_pulse'] = n_pulse
+								parsed_from_waveform['n_channel'] = n_channel
+								parsed_from_waveform = pandas.DataFrame(
+									parsed_from_waveform,
+									index = [0],
+								)
+								parsed_from_waveform.set_index(
 									['n_waveform','n_position','n_trigger','n_channel','n_pulse'],
-									inplace=True
+									inplace = True,
 								)
-								
-								waveforms_dumper.append(waveform_df)
-								extra_data_dumper.append(extra_data_df)
+								parsed_data_dumper.append(parsed_from_waveform)
 								
 								n_waveform += 1
 					if report_progress:
@@ -137,10 +142,10 @@ def plot_parsed_data_from_TCT_1D_scan(bureaucrat:RunBureaucrat, draw_main_plots:
 	Néstor = bureaucrat
 	
 	if strict_task_checking:
-		Néstor.check_these_tasks_were_run_successfully(['TCT_1D_scan','parse_waveforms'])
+		Néstor.check_these_tasks_were_run_successfully(['TCT_1D_scan'])
 	
 	with Néstor.handle_task('plot_parsed_data_from_TCT_1D_scan') as Néstors_employee:
-		parsed_data_df = load_whole_dataframe(Néstor.path_to_directory_of_task('parse_waveforms')/'parsed_from_waveforms.sqlite')
+		parsed_data_df = load_whole_dataframe(Néstor.path_to_directory_of_task('TCT_1D_scan')/'parsed_from_waveforms.sqlite')
 		measured_data_df = load_whole_dataframe(Néstor.path_to_directory_of_task('TCT_1D_scan')/'measured_data.sqlite')
 		
 		data_df = measured_data_df.merge(parsed_data_df, left_index=True, right_index=True)
@@ -185,52 +190,7 @@ def plot_parsed_data_from_TCT_1D_scan(bureaucrat:RunBureaucrat, draw_main_plots:
 					include_plotlyjs = 'cdn',
 				)
 
-def scan_and_parse(bureaucrat:RunBureaucrat, the_setup, delete_waveforms_file:bool, positions:list, acquire_channels:list, n_triggers_per_position:int=1, silent=True, reporter:TelegramReporter=None):
-	"""Perform a `TCT_1D_scan` and parse in parallel."""
-	Ernestino = bureaucrat
-	TCT_still_scanning = True
-	
-	def parsing_thread_function():
-		args = dict(
-			bureaucrat = Ernestino, 
-			name_of_task_that_produced_the_waveforms_to_parse = 'TCT_1D_scan',
-			silent = silent, 
-			continue_from_where_we_left_last_time = True,
-		)
-		while TCT_still_scanning:
-			try:
-				parse_waveforms(**args)
-			except:
-				pass
-			sleep(11)
-		parse_waveforms(**args) # This last call is in case there is a bunch of waveforms left.
-	
-	parsing_thread = threading.Thread(target=parsing_thread_function)
-	
-	try:
-		parsing_thread.start()
-		TCT_1D_scan(
-			bureaucrat = Ernestino, 
-			the_setup = the_setup, 
-			positions = positions, 
-			acquire_channels = acquire_channels, 
-			n_triggers_per_position = n_triggers_per_position, 
-			silent = silent,
-			reporter = reporter,
-		)
-	finally:
-		TCT_still_scanning = False
-		while parsing_thread.is_alive():
-			sleep(1)
-		
-		if delete_waveforms_file == True:
-			(Ernestino.path_to_directory_of_task('TCT_1D_scan')/'waveforms.sqlite').unlink()
-	
-	if not silent:
-		print(f'Doing some plots...')
-	plot_parsed_data_from_TCT_1D_scan(bureaucrat=Ernestino)
-
-def TCT_1D_scan_sweeping_bias_voltage(bureaucrat:RunBureaucrat, the_setup, delete_waveforms_file:bool, voltages:list, positions:list, acquire_channels:list, n_triggers_per_position:int=1, silent=True, reporter:TelegramReporter=None):
+def TCT_1D_scan_sweeping_bias_voltage(bureaucrat:RunBureaucrat, the_setup, voltages:list, positions:list, acquire_channels:list, n_triggers_per_position:int=1, silent=True, reporter:TelegramReporter=None):
 	"""Perform a several 1D scans with the TCT setup, one at each voltage.
 	
 	Arguments
@@ -270,9 +230,8 @@ def TCT_1D_scan_sweeping_bias_voltage(bureaucrat:RunBureaucrat, the_setup, delet
 						print('Setting bias voltage...')
 					the_setup.set_bias_voltage(volts=voltage)
 					Lorenzos_son = Lorenzos_employee.create_subrun(subrun_name=f'{Lorenzo.run_name}_{int(voltage)}V')
-					scan_and_parse(
+					TCT_1D_scan(
 						bureaucrat = Lorenzos_son,
-						delete_waveforms_file = delete_waveforms_file,
 						the_setup = the_setup,
 						positions = positions,
 						n_triggers_per_position = n_triggers_per_position,
@@ -283,63 +242,77 @@ def TCT_1D_scan_sweeping_bias_voltage(bureaucrat:RunBureaucrat, the_setup, delet
 							telegram_chat_id = my_telegram_bots.chat_ids['Robobot TCT setup'],
 						) if report_progress else None,
 					)
+					try:
+						plot_parsed_data_from_TCT_1D_scan(bureaucrat=Lorenzos_son)
+					except Exception:
+						pass
 					if not silent:
 						print(f'Finished {Lorenzos_son.run_name}.')
 					reporter.update(1)
 
-########################################################################
-
-# The following things are defined here such that they can be imported from other scripts.
-
-DEVICE_CENTER = {
-	'x': -3355.810546875e-6,
-	'y': 1565.5078125e-6,
-	'z': 71538.876953125e-6,
-}
-SCAN_STEP = 10e-6 # meters
-SCAN_LENGTH = 333e-6 # meters
-SCAN_ANGLE_DEG = 90 # deg
-
 if __name__ == '__main__':
+	
 	import my_telegram_bots
 	from configuration_files.current_run import Alberto
 	from utils import create_a_timestamp
 	from TheSetup import connect_me_with_the_setup
 	import os
 	
-	with Alberto.handle_task('TCT_scans', drop_old_data=False) as task_bureaucrat:
-		Mariano = task_bureaucrat.create_subrun(create_a_timestamp() + '_' + input('Measurement name? ').replace(' ','_'))
+	def create_list_of_positions(device_center_xyz:tuple, scan_length:float, scan_angle_deg:float, scan_step:float):
+		x = device_center_xyz[0] + np.arange(-scan_length/2,scan_length/2,scan_step)*np.cos(scan_angle_deg*np.pi/180)
+		y = device_center_xyz[1] + np.arange(-scan_length/2,scan_length/2,scan_step)*np.sin(scan_angle_deg*np.pi/180)
+		z = device_center_xyz[2] + 0*x + 0*y
+		return [(xx,yy,zz) for xx,yy,zz in zip(x,y,z)]
 	
-		x = DEVICE_CENTER['x'] + np.arange(-SCAN_LENGTH/2,SCAN_LENGTH/2, SCAN_STEP)*np.cos(SCAN_ANGLE_DEG*np.pi/180)
-		y = DEVICE_CENTER['y'] + np.arange(-SCAN_LENGTH/2,SCAN_LENGTH/2, SCAN_STEP)*np.sin(SCAN_ANGLE_DEG*np.pi/180)
-		z = DEVICE_CENTER['z'] + 0*x + 0*y
-		positions = []
-		for i in range(len(y)):
-			positions.append( [ x[i],y[i],z[i] ] )
-		
-		the_setup = connect_me_with_the_setup(who=f'iv_curve.py PID:{os.getpid()}')
-		
+	scans_config = pandas.read_csv('configuration_files/automatic_scans.csv').set_index('device_name')
+	scans_config['acquire_channels'] = scans_config['acquire_channels'].apply(lambda x: [int(n_channel) for n_channel in x.split(',')])
+	
+	preview_mode = input(f'Is this a preview? (yes/no) ')
+	
+	if preview_mode == 'yes':
+		print('Preview mode enabled!')
+		scans_config['scan_step (µm)'] = 10
+		scans_config['n_voltages'] = 1
+		scans_config['n_triggers_per_position'] = 11
+	elif preview_mode not in {'no',''}:
+		raise ValueError(f'Wrong answer!')
+	
+	the_setup = connect_me_with_the_setup(who=f'iv_curve.py PID:{os.getpid()}')
+	
+	with Alberto.handle_task('TCT_scans', drop_old_data=False) as task_bureaucrat:
 		with the_setup.hold_control_of_bias(), the_setup.hold_tct_control():
-			the_setup.set_current_compliance(amperes=10e-6)
-			the_setup.set_bias_output_status('on')
-			the_setup.set_bias_voltage(200)
-			the_setup.set_laser_DAC(660)
-			the_setup.set_laser_frequency(1000)
-			the_setup.set_laser_status('on')
-			
-			scan_and_parse(
-				bureaucrat = Mariano,
-				delete_waveforms_file = True,
-				# ~ voltages = interlace(np.linspace(100,220,33)),
-				the_setup = the_setup,
-				positions = positions,
-				n_triggers_per_position = 111,
-				acquire_channels = [1,4],
-				silent = False,
-				reporter = TelegramReporter(
-					telegram_token = my_telegram_bots.robobot.token, 
-					telegram_chat_id = my_telegram_bots.chat_ids['Robobot TCT setup'],
-				),
-			)
-			
-			the_setup.set_bias_output_status('off')
+			try:
+				for device_name in scans_config.index:
+					Mariano = task_bureaucrat.create_subrun(create_a_timestamp() + '_' + f'{device_name}_TCT1DScansSweepingV' + ('_preview' if preview_mode else ''))
+				
+					the_setup.set_current_compliance(amperes=10e-6)
+					the_setup.set_bias_output_status('on')
+					the_setup.set_laser_DAC(660)
+					the_setup.set_laser_frequency(1000)
+					the_setup.set_laser_status('on')
+					
+					TCT_1D_scan_sweeping_bias_voltage(
+						bureaucrat = Mariano,
+						voltages = interlace(np.linspace(
+							scans_config.loc[device_name,'highest_voltage (V)'],
+							scans_config.loc[device_name,'lowest_voltage (V)'],
+							scans_config.loc[device_name,'n_voltages'],
+						)),
+						the_setup = the_setup,
+						positions = create_list_of_positions(
+							device_center_xyz = tuple([scans_config.loc[device_name,coord]*1e-6 for coord in ('x_center (µm)','y_center (µm)','z_center (µm)')]),
+							scan_length = scans_config.loc[device_name,'scan_length (µm)']*1e-6,
+							scan_angle_deg = scans_config.loc[device_name,'scan_angle (deg)'],
+							scan_step = scans_config.loc[device_name,'scan_step (µm)']*1e-6,
+						),
+						n_triggers_per_position = scans_config.loc[device_name,'n_triggers_per_position'],
+						acquire_channels = scans_config.loc[device_name,'acquire_channels'],
+						silent = False,
+						reporter = TelegramReporter(
+							telegram_token = my_telegram_bots.robobot.token, 
+							telegram_chat_id = my_telegram_bots.chat_ids['Robobot TCT setup'],
+						),
+					)
+			finally:	
+				the_setup.set_bias_output_status('off')
+				the_setup.set_laser_status('on')

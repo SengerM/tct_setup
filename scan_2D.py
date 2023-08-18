782136e2342941aa161e15429b942849c8a0f5d7
Matias Senger <m.senger@hotmail.com>
2023-08-18 14:31:35 +0200

    Implemented rotation of coordinate system for tilted devices, improved plots

--- scan_2D.py
+++ scan_2D.py
@@ -87,28 +87,69 @@ def plot_everything_from_TCT_2D_scan(bureaucrat:RunBureaucrat):
 	bureaucrat.check_these_tasks_were_run_successfully('TCT_2D_scan')
 	
 	with bureaucrat.handle_task('plot_everything_from_TCT_2D_scan') as employee:
+		logging.info(f'Reading data from {repr(bureaucrat.run_name)}...')
 		if len(bureaucrat.list_subruns_of_task('TCT_2D_scan')) != 1:
 			raise RuntimeError(f'Run {repr(bureaucrat.run_name)} located in "{bureaucrat.path_to_run_directory}" seems to be corrupted because I was expecting only a single subrun for the task "TCT_2D_scan" but it actually has {len(bureaucrat.list_subruns_of_task("TCT_2D_scan"))} subruns...')
 		flattened_1D_scan_subrun_bureaucrat = bureaucrat.list_subruns_of_task('TCT_2D_scan')[0]
-		parsed_from_waveforms = load_whole_dataframe(flattened_1D_scan_subrun_bureaucrat.path_to_directory_of_task('TCT_1D_scan')/'parsed_from_waveforms.sqlite')
+		data = load_whole_dataframe(flattened_1D_scan_subrun_bureaucrat.path_to_directory_of_task('TCT_1D_scan')/'parsed_from_waveforms.sqlite')
+		
 		positions_data = pandas.read_pickle(bureaucrat.path_to_directory_of_task('TCT_2D_scan')/'positions.pickle')
 		positions_data.reset_index(['n_x','n_y'], drop=False, inplace=True)
+		for _ in {'x','y'}: # Remove offset so (0,0) is the center...
+			positions_data[f'{_} (m)'] -= positions_data[f'{_} (m)'].mean()
+		
+		# ~ utils.create_parallel_xy_grid_from_tilted_xy_grid(positions_data)
 
-		data = parsed_from_waveforms.merge(positions_data, left_index=True, right_index=True)
-		data.set_index(['n_x','n_y'], append=True, inplace=True)
-		for _ in {'x','y'}:
-			data[f'{_} (m)'] -= data[f'{_} (m)'].mean()
+		data = data.query('n_pulse==1')
+		averages = data.groupby(['n_position','n_channel']).agg(numpy.nanmedian)
 		
-		averages = data.groupby(['n_pulse','n_channel','n_x','n_y']).agg(numpy.nanmedian)
-		averages = averages.query('n_pulse==1')
+		averages = averages.merge(positions_data, left_index=True, right_index=True)
 		
+		# Plot as function of nx,ny:
+		logging.info('Producing plots as function of n_x,n_y...')
+		averages.reset_index(inplace=True, drop=False)
+		averages.set_index(['n_y','n_x','n_channel'], inplace=True)
+		path_for_nx_ny_plots = employee.path_to_directory_of_my_task/'nx_ny'
+		path_for_nx_ny_plots.mkdir()
+		for col in averages.columns.get_level_values(0).drop_duplicates():
+			if col in {'n_position'}:
+				continue
+			data_to_plot_xarray = averages[col].to_xarray()
+			fig = px.imshow(
+				title = f'{col}<br><sup>{bureaucrat.run_name}</sup>',
+				img = data_to_plot_xarray,
+				aspect = 'equal',
+				facet_col = 'n_channel',
+				width = 555*len(data_to_plot_xarray.coords['n_channel']),
+				origin = 'lower',
+			)
+			fig.update_coloraxes(colorbar_title_side='right')
+			fig.write_html(
+				path_for_nx_ny_plots/f'{col}_nx_ny.html',
+				include_plotlyjs = 'cdn',
+			)
+		fig = px.imshow(
+			title = f'sum(Amplitude (V))<br><sup>{bureaucrat.run_name}</sup>',
+			img = averages['Amplitude (V)'].groupby(['n_y','n_x']).sum().to_xarray(),
+			aspect = 'equal',
+			origin = 'lower',
+		)
+		fig.update_coloraxes(colorbar_title_side='right')
+		fig.write_html(
+			path_for_nx_ny_plots/f'sum(Amplitude (V))_nx_ny.html',
+			include_plotlyjs = 'cdn',
+		)
+		
+		# Plot as function of x,y:
+		logging.info('Producing plots as a function of x,y...')
 		xy_table = pandas.pivot_table(
 			data = averages,
 			values = averages.columns,
 			index = ['y (m)','n_channel'],
 			columns = 'x (m)',
 		)
-		
+		path_for_x_y_plots = employee.path_to_directory_of_my_task/'xy'
+		path_for_x_y_plots.mkdir()
 		for col in set(xy_table.columns.get_level_values(0)):
 			numpy_array = numpy.array([numpy.flip(xy_table[col].query(f'n_channel=={n_channel}').to_numpy(),axis=1) for n_channel in sorted(set(xy_table[col].index.get_level_values('n_channel')))])
 			fig = px.imshow(
@@ -130,10 +171,12 @@ def plot_everything_from_TCT_2D_scan(bureaucrat:RunBureaucrat):
 			for i,n_channel in enumerate(sorted(set(xy_table[col].index.get_level_values('n_channel')))):
 				fig.layout.annotations[i].update(text=f'n_channel:{n_channel}')
 			fig.write_html(
-				employee.path_to_directory_of_my_task/f'{col}.html',
+				path_for_x_y_plots/f'{col}.html',
 				include_plotlyjs = 'cdn',
 			)
 		
+		# Plot some histograms:
+		logging.info('Producing histogram plots...')
 		for col in {'t_50 (s)','Amplitude (V)'}:
 			fig = go.Figure()
 			for n_channel in sorted(set(data.index.get_level_values('n_channel'))):
@@ -152,7 +195,7 @@ def plot_everything_from_TCT_2D_scan(bureaucrat:RunBureaucrat):
 				employee.path_to_directory_of_my_task/f'{col}_histogram.html',
 				include_plotlyjs = 'cdn',
 			)
-			
+	logging.info('Finished plotting 2D scan!')
 
 def TCT_2D_scans_sweeping_bias_voltage(bureaucrat:RunBureaucrat, the_setup, voltages:list, positions:list, acquire_channels:list, n_triggers_per_position:int=1, reporter:SafeTelegramReporter4Loops=None, compress_waveforms_files:bool=True):
 	bureaucrat.create_run(if_exists='skip')
@@ -205,8 +248,9 @@ if __name__ == '__main__':
 	Y_SPAN = X_SPAN
 	X_STEP = 25e-6
 	Y_STEP = X_STEP
-	DEVICE_NAME = 'CNM_AC-LGAD_testing_CAEN'
+	DEVICE_NAME = 'AC70'
 	DEVICE_CENTER = (-3817e-6,1914e-6+200e-6,74292e-6)
+	ROTATION_ANGLE_DEG = 45
 	VOLTAGES = [111]
 	LASER_DAC = 111
 	N_TRIGGERS_PER_POSITION = 22
@@ -221,15 +265,22 @@ if __name__ == '__main__':
 	
 	set_my_template_as_default()
 	
-	def create_list_of_positions(device_center_xyz:tuple, x_span:float, y_span:float, x_step:float, y_step:float, readout_pads_to_remove:dict=None):
+	def create_list_of_positions(device_center_xyz:tuple, x_span:float, y_span:float, x_step:float, y_step:float, rotation_angle_deg:float, readout_pads_to_remove:dict=None):
 		x = numpy.linspace(-x_span/2, x_span/2, int(x_span/x_step+1))
 		y = numpy.linspace(-y_span/2, y_span/2, int(y_span/y_step+1))
 		
 		xx, yy = numpy.meshgrid(x, y)
-		zz = xx*0 + device_center_xyz[2]
+		
+		# Apply rotation
+		phi = numpy.arctan2(yy,xx)
+		cos = numpy.cos(rotation_angle_deg*numpy.pi/180+phi)
+		sin = numpy.sin(rotation_angle_deg*numpy.pi/180+phi)
+		rr = (xx**2+yy**2)**.5
+		xx, yy = rr*cos, rr*sin
 		
 		xx += device_center_xyz[0]
 		yy += device_center_xyz[1]
+		zz = xx*0 + device_center_xyz[2]
 		
 		remove_these = numpy.full(xx.shape, False)
 		if isinstance(readout_pads_to_remove, dict):
@@ -279,6 +330,7 @@ if __name__ == '__main__':
 						x_step = X_STEP,
 						y_step = Y_STEP,
 						readout_pads_to_remove = REMOVE_PADS,
+						rotation_angle_deg = ROTATION_ANGLE_DEG,
 					),
 					acquire_channels = list(range(16)),
 					n_triggers_per_position = N_TRIGGERS_PER_POSITION,
@@ -286,7 +338,7 @@ if __name__ == '__main__':
 						bot_token = my_telegram_bots.robobot.token, 
 						chat_id = my_telegram_bots.chat_ids['Robobot TCT setup'],
 					),
-					compress_waveforms_files = False,
+					compress_waveforms_files = True,
 				)
 			finally:
 				logging.info('Finalizing scan...')
